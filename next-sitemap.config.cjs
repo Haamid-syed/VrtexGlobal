cf1441ee056dbb1d00a49b296c762e24b6b464ab
Haamid-syed <jaguarv999@gmail.com>
2026-01-18 14:27:58 +0530

    Fix canonical siteUrl to www domain

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: process.env.SITE_URL || "https://VrtexGlobal.com",
+  siteUrl: "https://www.vrtexglobal.com",
   generateRobotsTxt: true,
   generateIndexSitemap: true,
   sitemapSize: 7000,
